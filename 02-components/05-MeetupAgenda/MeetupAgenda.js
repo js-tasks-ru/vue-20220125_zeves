6097c827d67e9db88898ba6abb631aaff6937b90
Kirill Lavrov <zeves095@gmail.com>
2022-02-11 12:42:05 +0300

    ready 2.5

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,18 @@
-import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import {defineComponent} from './vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
-  template: `
+  props: {
+    agenda: Array
+  },
+
+  components: {MeetupAgendaItem},
+  template  : `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
-      </li>
+    <li v-for="item in agenda" class="agenda__item">
+      <meetup-agenda-item :agenda-item="item" />
+    </li>
     </ul>`,
 });
